5cbbd93694fc3c7d3b5d115aab243fc39ae9efc3
Leandromart2003 <leandro.mart.2003@gmail.com>
2026-04-13 15:18:58 +0100

    Update main.py

--- Exercícios/ExercíciosREGEX/main.py
+++ Exercícios/ExercíciosREGEX/main.py
@@ -21,3 +21,21 @@ def validar_email(email):
         return True
     else:
         return False
+        
+def extrair_dominio(email):
+    padrao_dominio = r'@([\w\.-]+)$'
+    resultado = re.search(padrao_dominio, email)
+    if resultado:
+        return resultado.group(1)
+    else:
+        return None
+    
+
+def validar_nifs(nif):
+    padrao_nif = r'^[123568]\d{8}$'
+    if re.match(padrao_nif, nif):
+        return True
+    else:
+        return False
+    
+
